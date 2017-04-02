6887615aa9be8d4a4b8a78ea0879330f3b671b4b
Kenneth Reinhardt <kenneth@reinhardt.dk>
2017-04-02 11:18:40 +0200

    Routes working

--- module4-solution/src/routes.js
+++ module4-solution/src/routes.js
@@ -1,33 +1,33 @@
 (function () {
+'use strict';
 
-angular.module('MenuApp',['ui.router']);
+angular.module('MenuApp', ['ui.router']);
 
-angular.module('MenuApp')
-.config(RoutesConfig);
+angular.module('MenuApp').config(RoutesConfig);
 
-RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-function RoutesConfig($stateProvider, $urlRouterProvider) {
-
-  // Redirect to tab 1 if no other URL matches
-  $urlRouterProvider.otherwise('/tab1');
-
-  // Set up UI states
-  $stateProvider
-    .state('tab1', {
-      url: '/tab1',
-      templateUrl: 'src/tab1.html'
-    })
-
-    .state('tab2', {
-      url: '/tab2',
-      templateUrl: 'src/tab2.html'
-    })
-    
-    .state('tab3', {
-      url: '/tab3',
-      templateUrl: 'src/tab3.html'
-    });
-}
+  RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function RoutesConfig($stateProvider, $urlRouterProvider) {
+  
+    // Redirect to tab 1 if no other URL matches
+    $urlRouterProvider.otherwise('/tab1');
+  
+    // Set up UI states
+    $stateProvider
+      .state('tab1', {
+        url: '/home',
+        templateUrl: 'src/tab1.html'
+      })
+  
+      .state('tab2', {
+        url: '/categories',
+        templateUrl: 'src/tab2.html'
+      })
+  
+      .state('tab3', {
+        url: '/items',
+        templateUrl: 'src/tab3.html'
+      });
+  }
 
 
 })();
